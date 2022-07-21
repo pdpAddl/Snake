b21b0f11f14c7ec660b949622e2a9a4a1f89068f
Patrick Schaible <patrick.schaible1@gmail.com>
2022-07-21 21:06:43 +0200

    Basic window with no functionality

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+
+#include "SFML//System.hpp"
 #include "SFML/Graphics.hpp"
 
 
@@ -6,6 +8,44 @@ using namespace std;
 
 int main()
 {
-	cout << "Test";
+	//Window
+	sf::RenderWindow window(sf::VideoMode(640, 480), "Snake", sf::Style::Titlebar | sf::Style::Close);
+	sf::Event ev;
+
+	//Game loop
+	while (window.isOpen())
+	{
+		//Event polling
+		while (window.pollEvent(ev))
+		{
+			switch (ev.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (ev.key.code == sf::Keyboard::Escape)
+				{
+					window.close();
+				}
+				break;
+
+			}
+		}
+
+		//Update
+
+
+		//Render
+		window.clear(sf::Color::Cyan); //Clear old frame
+
+
+
+		window.display(); // Window is done drawing
+
+
+	}
+
+
 	return 0;
 }
